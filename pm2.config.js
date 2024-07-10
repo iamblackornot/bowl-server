de814f85d5fab4ad55cd7058fa842b5328332bf8
iamblackornot <nkzshinnnik@gmail.com>
2024-07-10 19:19:20 +0300

    fixed pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: 'bowl-server',
-    script: '/usr/lib/node_modules/ts-node/dist/bin.js',  // Path to ts-node executable
-    args: 'src/index.ts',                      // Replace with the entry file of your TypeScript app
-    watch: false,                              // Disable watching for production environments
-    exec_mode: 'fork',                         // Use fork mode for a single instance
-    instances: 1,                              // Limit to one instance
+    script: 'npx',
+    args: 'src/index.ts',
+    watch: false,
+    exec_mode: 'fork',
+    instances: 1,
   };
